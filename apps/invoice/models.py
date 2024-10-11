2925430b939de98504d6f10c3e95b09850fbb312
Dalnec <dalnec1405@gmail.com>
2024-10-10 21:25:51 -0500

    Period in Invoice

--- apps/invoice/models.py
+++ apps/invoice/models.py
@@ -1,3 +1,4 @@
+import locale
 from django.db import models
 from model_utils.models import TimeStampedModel
 
@@ -25,3 +26,8 @@ class Invoice(TimeStampedModel):
 
     def __str__(self):
         return f"{self.id} - {self.usuario} - {self.measured}"
+    
+    @property
+    def period(self):
+        locale.setlocale(locale.LC_TIME, '')
+        return self.read_date.strftime('%B %Y')
--- apps/invoice/serializers.py
+++ apps/invoice/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import *
 
 class InvoiceSerializer(serializers.ModelSerializer):
+    period = serializers.SerializerMethodField()
     class Meta:
         model = Invoice
         fields = '__all__'
+    
+    def get_period(self, obj):
+        return obj.period
