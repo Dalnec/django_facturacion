31ccbf300ead028a8cf1d3d16c29312dd5422ae4
Dalnec <dalnec1405@gmail.com>
2024-10-24 18:11:41 -0400

    employee_name

--- apps/purchase/serializers.py
+++ apps/purchase/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import *
 
 class PurchaseSerializer(serializers.ModelSerializer):
+    employee_name = serializers.ReadOnlyField(source="employee.names")
     class Meta:
         model = Purchase
         fields = '__all__'
