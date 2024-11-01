6c5c939364305ae8865aff9e34a0714989ea2599
Dalnec <dalnec1405@gmail.com>
2024-11-01 09:51:59 -0400

    get_purchases action

--- apps/purchase/views.py
+++ apps/purchase/views.py
@@ -49,3 +49,9 @@ class PurchaseView(viewsets.GenericViewSet):
         instance = self.get_object()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, methods=['get'])
+    def get_purchases(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
