7c366e6cd8693003510e4d91695415d03a4caff9
Dalnec <dalnec1405@gmail.com>
2024-11-04 19:00:32 -0400

    measuring fields changes

--- apps/monitoring/models.py
+++ apps/monitoring/models.py
@@ -11,9 +11,9 @@ class Monitoring(TimeStampedModel):
     )
     
     read_date = models.DateTimeField("Fecha Lectura", blank=True, null=True)
-    measured = models.DecimalField("Lectura Sensor", max_digits=18, decimal_places=2, blank=True, null=True)
-    percentage = models.CharField("Porcentaje", max_length=5, blank=True, null=True)
-    battery = models.CharField("Nivel Bateria", max_length=5, blank=True, null=True)
+    measured = models.DecimalField("Lectura Sensor", max_digits=18, decimal_places=2)
+    percentage = models.CharField("Porcentaje", max_length=5)
+    battery = models.CharField("Nivel Bateria", max_length=5)
     status = models.CharField("Estado", max_length=1, choices=STATUS_CHOICES, default='C')
     observations = models.CharField('Observaciones', max_length=255, blank=True, null=True)
 
