4bba3c113c9900771a8483a7007574f7b5b2c4cc
Dalnec <dalnec1405@gmail.com>
2024-11-27 08:55:10 -0400

    base.py

--- core/settings/local.py
+++ core/settings/local.py
@@ -10,15 +10,15 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS  = [
     'http://localhost:5173',
     'http://localhost:8000',
-    'http://*.tsifactur.com',
-    'https://*.tsifactur.com',
+    'http://*.tsifactur.com/',
+    'https://*.tsifactur.com/',
 ]
  
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:8000',
-    'http://*.tsifactur.com',
-    'https://*.tsifactur.com',
+    'http://*.tsifactur.com/',
+    'https://*.tsifactur.com/',
 ]
 
 DATABASES = {
