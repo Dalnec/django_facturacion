9a6187ccec547e2654712896d192a94024224752
Dalnec <dalnec1405@gmail.com>
2024-11-26 21:13:08 -0400

    test report invoices

--- apps/invoice/views.py
+++ apps/invoice/views.py
@@ -10,7 +10,9 @@ from apps.purchase.models import Purchase
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.db import transaction
-from django.db.models import Sum
+from django.db.models import Sum, Value, F, Subquery, OuterRef, TextField, When, Case
+from django.db.models.functions import Concat
+from django.contrib.postgres.aggregates import StringAgg
 # from io import BytesIO
 import io
 from django.template.loader import render_to_string
@@ -167,6 +169,31 @@ class InvoiceView(viewsets.GenericViewSet):
         # params = request.GET.dict()
         params = request.query_params.dict()
         queryset = self.filter_queryset(self.get_queryset().order_by('-id'))
+        # concatenar en observaciones los detalles de la factura
+        # for invoice in queryset:
+        #     details = invoice.fk_usuariodetail_invoice.all()
+        #     if details.exists():
+        #         for detail in details:
+        #             invoice.observaciones = f"{invoice.observations} | {detail.description} {detail.subtotal}"
+        queryset = queryset.annotate(
+            details=Concat(
+                Case(
+                    When(observations__isnull=True, then=Value('')),  # Si es None, usa una cadena vacía
+                    default=F('observations'),  # Usa el valor de observations si no es None
+                    output_field=TextField()
+                ),
+                Value(' | '),  # Separador
+                Subquery(
+                    UsuarioDetail.objects.filter(invoice=OuterRef('id'))
+                    .values('invoice')
+                    .annotate(
+                        detalles_concat=StringAgg('description', delimiter=' | ')
+                    )
+                    .values('detalles_concat')
+                ),
+                output_field=TextField()
+            )
+        )
         data = {
             "invoices": queryset,
             "counter": 0,
