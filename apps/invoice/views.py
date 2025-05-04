83aec679ad8f6257d4847f09326eeefb38e8d135
Dalnec <dalnec1405@gmail.com>
2025-05-04 11:08:16 -0400

    penalty al crear invoice 2

--- apps/invoice/views.py
+++ apps/invoice/views.py
@@ -97,7 +97,7 @@ class InvoiceView(viewsets.GenericViewSet):
                     UsuarioDetail.generate_mora(usuario, distric.settings["penalty_amount"], invoice)
 
             # INFO: Actualiza el 'detalle del usuario' donde estado debe ser true para agregar al detalle del recibo
-            detail = invoice.usuario.fk_usuariodetail_usuario.filter(invoice__isnull=True, status=True)
+            detail = invoice.usuario.fk_usuariodetail_usuario.filter(invoice__isnull=True, invoice__id=invoice.id, status=True)
             # INFO: Actualiza total de invoice en caso tenga detalle
             if detail.exists():
                 income = detail.filter(is_income=True).aggregate(total=Sum('subtotal'))['total'] or 0
